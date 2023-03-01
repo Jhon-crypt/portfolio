41983a3b57ea6efa8f6a6047b8de3458f7b867fe
Jhon-crypt <johnoladele690@gmail.com>
2023-03-01 19:52:11 +0100

    Updated projects page

--- src/pages/projects.page.tsx
+++ src/pages/projects.page.tsx
@@ -5,14 +5,15 @@ import image2 from '../images/taskstack.png'
 import image3 from '../images/wheelsx.netlify.app_.png'
 import image4 from '../images/phoneshopz.png'
 import image5 from '../images/recap-app.netlify.app_.png'
+import image6 from '../images/imagex.png'
 
 
-function Projects(): JSX.Element{
+function Projects(): JSX.Element {
 
     return (
 
         <>
-        <br /><br />
+        <br /> < br / >
         <div id="project" className="mt-5 py-5 pb-5 container text-light">
 
             <h3 className="text-center display-5 fw-bold">
@@ -152,11 +153,37 @@ function Projects(): JSX.Element{
 
                 </div>
 
-            </div>
+                <div className="col">
+
+                    <div className="card bg-dark mb-2">
+
+                        <img className="crad-img-top" src={image6}
+                        style={{"objectFit":"cover","height":"150px"}} alt="logo"/>
+
+                        <div className="card-img-overlay" style={{"fontWeight":"bold","border":"1px solid",
+                        "backgroundColor": "rgba(0,0,0,0.4)"}}>
+
+                            <h4 className="card-title">
+                                ImageX
+                            </h4>
+
+                            <p>Generate images from texts</p>
+
+                            <a href="https://recap-app.netlify.app/" className="btn btn-light text-dark">
+                                View <i className="fa fa-external-link"></i>
+                            </a>
+
+                        </div>
+
+                    </div>
+
+                </div>
 
             </div>
 
-        </div>
+            </div>
+
+        </div> 
         </>
 
     )
